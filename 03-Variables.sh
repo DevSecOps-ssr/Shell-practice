ccb29ed353c202d54eb5d1110af2bcbbe720f630
Sivaram-ssr <sanganilakshmisivaram@gmail.com>
2026-02-04 13:24:57 +0530

    new changes are added

---
+++ 03-Variables.sh
@@ -0,0 +1,7 @@
+PERSON_1=Siva
+PERSON_2=Ram
+
+echo "$PERSON_1:: Hello $PERSON_2, How are you?"
+echo "$PERSON_2:: Hi $PERSON_1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON_1:: doing fine. What are you learning $PERSON_2?"
+echo "$PERSON_2:: I am learning Shell. What about you?"
