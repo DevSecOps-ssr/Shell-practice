fc337d3fd0e59a4020b851eebc329294f73b87eb
Sivaram-ssr <sanganilakshmisivaram@gmail.com>
2026-02-04 13:12:08 +0530

    new changes are added

--- 02-Conversation.sh
+++ 02-Conversation.sh
@@ -1,4 +1,4 @@
 echo "Siva:: Hello Ram, How are you?"
 echo "Ram:: Hi Siva, I am fine thanks for asking. How are you doing?"
-echo "Siva:: doing fine. What are you learning Rame?"
+echo "Siva:: doing fine. What are you learning Ram?"
 echo "Ram:: I am learning Shell. What about you?"
